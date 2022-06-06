f3787b75f4eec4715e75a70fc5becd8827cf113d
SubashGitLogin <subash007official@gmail.com>
2022-06-06 07:54:01 -0700

    Some Codes Added

--- src/org/sample/ExampleGit.java
+++ src/org/sample/ExampleGit.java
@@ -6,8 +6,13 @@ public class ExampleGit {
 		System.out.println("Code Number 1");
 
 	}
+	private void code2() {
+		System.out.println("Code Number 2");
+	}
 public static void main(String[] args) {
-	
+	ExampleGit e=new ExampleGit();
+	e.code1();
+	e.code2();
 	
 	}
 }
